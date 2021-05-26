60aac674fe93edea5583a30342e58cd197939912
s060071 <ipuicheung@gmail.com>
2021-05-26 15:21:51 +0800

    final edit 1

--- react-assessment(test)/src/Components/App/App.js
+++ react-assessment(test)/src/Components/App/App.js
@@ -1,30 +1,31 @@
-import React from 'react';
+import React from "react";
 import './App.css';
+import {SearchBar} from '../SearchBar/SearchBar';
+import {SearchResults} from '../SearchResults/SearchResults';
+import {Playlist} from '../Playlist/Playlist';
+import {TrackList} from '../TrackList/TrackList';
+import {Track} from '../Track/Track';
 
 class App extends React.Component {
         render() {
-            return ( <
-                div >
-                <
-                h1 > Ja < span className = "highlight" > mmm < /span>ing</h
-                1 >
-                <
-                div className = "App" >
-
-                < //SearchBar />
-
-                <
-                div className = "App-playlist" >
-
-                // 
-                //<SearchResults / >
-
-                // <Playlist / >
-                <
-                /div> <
-                /div>  <
-                /div>
+            return ( 
+                <div>
+                    <h1>Ja<span className="highlight">mmm</span>ing</h1>
+                    <div className="App">
+                    {/* <!-- Add a SearchBar component --> */}
+                        <SearchBar />
+                        <div className="App-playlist">
+                    {/* <!-- Add a SearchResults component --> */}
+                        <SearchResults />
+                    {/* <!-- Add a Playlist component --> */}
+                        <Playlist />
+                        <TrackList />
+                        <Track/>
+                        </div>
+                    </div>
+              </div>
             )
-        }
+        }                   
+}
 
-        export default App;
+export default App;
